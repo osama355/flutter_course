1f5ed5417e70d4bc5ea446a01fa7b40e853a9b48
FlutLab.io <>
2021-05-19 09:29:08 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
               title: Text('My First App'),
               backgroundColor: Colors.blue,
             ),
-            body: Row(Image.network('https://www.google.com/search?q=laptop+image&sxsrf=ALeKk00cb4N8PiW_RgBHTRJN8g0dx56MYA:1621416167145&tbm=isch&source=iu&ictx=1&fir=0vRBGtHIRtMF9M%252CcMfgFMspuOTiHM%252C_&vet=1&usg=AI4_-kSrzCeoxcIbwdxUk3lXWiL-jWjYMQ&sa=X&ved=2ahUKEwjGzbP1tdXwAhWJZxUIHT5tAXAQ9QF6BAgHEAE#imgrc=0vRBGtHIRtMF9M'),
-            Center(child: Text('Hello from Osama')))
-            );
+            body: Row(children:[Image.network('https://www.google.com/search?q=laptop+image&sxsrf=ALeKk00cb4N8PiW_RgBHTRJN8g0dx56MYA:1621416167145&tbm=isch&source=iu&ictx=1&fir=0vRBGtHIRtMF9M%252CcMfgFMspuOTiHM%252C_&vet=1&usg=AI4_-kSrzCeoxcIbwdxUk3lXWiL-jWjYMQ&sa=X&ved=2ahUKEwjGzbP1tdXwAhWJZxUIHT5tAXAQ9QF6BAgHEAE#imgrc=0vRBGtHIRtMF9M'),
+            Center(child: Text('Hello from Osama'))])
+        );
   }
 }
