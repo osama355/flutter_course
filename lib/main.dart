9c513b9eea15086cf6aba93e0e4eb708f297de1c
FlutLab.io <>
2021-05-19 09:22:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   return runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar:AppBar(
-          title:Text('My First App'),
-          backgroundColor: Colors.blue,
-        ),
-        body:Center(child:Text('Hello from Osama'))
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('My First App'),
+              backgroundColor: Colors.blue,
+            ),
+            body: Center(child: Text('Hello from Osama'))
+          ));
   }
 }
