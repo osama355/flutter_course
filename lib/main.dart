f0b01019d36b7a7666666fa025742bcccad2df37
FlutLab.io <>
2021-05-19 08:35:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
           title:Text('My First App'),
           backgroundColor: Colors.blue,
         ),
-        body:Center(Text('Hello from Osama'))
+        body:Center(child:Text('Hello from Osama'))
       )
     );
   }
