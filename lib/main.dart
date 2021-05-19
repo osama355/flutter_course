d473bed91cd4803758d008eab2d51ec55ad71c61
FlutLab.io <>
2021-05-19 09:24:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.blue,
             ),
             body: Image.network('https://www.google.com/search?q=laptop+image&sxsrf=ALeKk00cb4N8PiW_RgBHTRJN8g0dx56MYA:1621416167145&tbm=isch&source=iu&ictx=1&fir=0vRBGtHIRtMF9M%252CcMfgFMspuOTiHM%252C_&vet=1&usg=AI4_-kSrzCeoxcIbwdxUk3lXWiL-jWjYMQ&sa=X&ved=2ahUKEwjGzbP1tdXwAhWJZxUIHT5tAXAQ9QF6BAgHEAE#imgrc=0vRBGtHIRtMF9M'),
-            Center(child: Text('Hello from Osama'))));
+            Center(child: Text('Hello from Osama')))
+            );
   }
 }
